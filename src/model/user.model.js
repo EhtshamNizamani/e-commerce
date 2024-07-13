08c001dd0f1f9253f37d46b961cdcc73d2aa226e
Ehtsham <114107331+EhtshamNizamani@users.noreply.github.com>
2024-07-13 16:19:52 +0500

    created user model

---
+++ src/model/user.model.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+const Schema = mongoose.Schema;
+
+// Define the User Schema
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["user", "admin"],
+    default: "user",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordValid = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+// Create and export the User model
+module.exports = mongoose.model("User", userSchema);
