2cf07695642845f065fac4793ebdcbb70254b657
Ehtsham <48251967+HassanButt2019@users.noreply.github.com>
2024-07-28 16:19:25 +0500

    created add cart remove from cart and minus quantity of a item

--- src/controller/cart.controller.js
+++ src/controller/cart.controller.js
@@ -4,38 +4,38 @@ import { ApiResponse } from "../utils/apiResponse.js";
 import { Cart } from "../model/cart.model.js";
 import { User } from "../model/user.model.js";
 import { Product } from "../model/product.model.js";
-
 const addToCart = asyncHandler(async (req, res) => {
   const { product_id } = req.params;
+  const { quantity } = req.body;
+
   const product = await Product.findById(product_id);
 
   if (!product) {
     throw new ApiError(404, "Product not found");
   }
 
-  let cart = await Cart.findOne({ user: req.user._id });
+  let cart = await Cart.findOne({ user: req.user?._id });
 
   if (cart) {
-    // Check if item is already in cart
-    const itemIndex = cart.items.findIndex(
+    const existingItemIndex = cart.items.findIndex(
       (item) => item.productId.toString() === product_id
     );
-    if (itemIndex > -1) {
-      // Item already in cart, update quantity
-      cart.items[itemIndex].quantity += 1;
+
+    if (existingItemIndex !== -1) {
+      // Update the quantity of the existing product
+      cart.items[existingItemIndex].quantity += quantity;
     } else {
-      // Item not in cart, add new item
-      cart.items.push({ productId: product_id, quantity: 1 });
+      // Add a new item to the cart
+      cart.items.push({ productId: product_id, quantity });
     }
+    await cart.save();
   } else {
-    // No cart for user, create new cart
-    cart = new Cart({
-      user: req.user._id,
-      items: [{ productId: product_id, quantity: 1 }],
+    // Create a new cart if it doesn't exist
+    cart = await Cart.create({
+      user: req.user?._id,
+      items: [{ productId: product_id, quantity }],
     });
   }
-  console.log(cart);
-  await cart.save();
 
   res
     .status(200)
@@ -44,21 +44,57 @@ const addToCart = asyncHandler(async (req, res) => {
 
 const getCartProduct = asyncHandler(async (req, res) => {
   const userId = req.user._id;
-  const cart = await Cart.aggregate([
-    {
-      $match: {
-        user: userId,
-      },
-    },
-  ]);
-
-  if (!cart) {
-    throw new ApiError(404, "Cart not found!");
-  }
-
+  const cart = await Cart.findOne({ user: userId })
+    .populate({ path: "items.productId", select: "title price" })
+    .exec();
   res
     .status(200)
-    .json(new ApiResponse(200, cart[0], "Cart retrieved successfully"));
+    .json(new ApiResponse(200, cart, "Cart retrieved successfully"));
 });
 
-export { addToCart, getCartProduct };
+const removeCartProduct = asyncHandler(async (req, res) => {
+  const userId = req.user._id;
+  const { product_id } = req.params;
+  const cart = await Cart.findOneAndDelete(
+    {
+      user: userId,
+      "items.productId": product_id,
+    },
+    {
+      new: true,
+    }
+  );
+
+  res.status(200).json(new ApiResponse(200, cart, "Cart deleted successfully"));
+});
+
+const removeQuantityOfProduct = asyncHandler(async (req, res) => {
+  const userId = req.user._id;
+  const { product_id } = req.params;
+
+  let cart = await Cart.findOne({ user: userId });
+
+  if (cart) {
+    const existingItemIndex = cart.items.findIndex(
+      (item) => item.productId.toString() === product_id
+    );
+    if (existingItemIndex !== -1) {
+      if (cart.items[existingItemIndex].quantity === 1) {
+        cart.items.splice(existingItemIndex, 1);
+      } else {
+        // Update the quantity of the existing product
+        cart.items[existingItemIndex].quantity -= 1;
+      }
+    }
+    await cart.save();
+  } else {
+    res.status(404).json(new ApiError(404, "Product not found"));
+  }
+  res.status(200).json(new ApiResponse(200, cart, "Cart minus the quantity"));
+});
+export {
+  addToCart,
+  getCartProduct,
+  removeCartProduct,
+  removeQuantityOfProduct,
+};
--- src/routes/cart.route.js
+++ src/routes/cart.route.js
@@ -1,10 +1,18 @@
 import { Router } from "express";
-import { addToCart, getCartProduct } from "../controller/cart.controller.js";
+import {
+  addToCart,
+  getCartProduct,
+  removeCartProduct,
+  removeQuantityOfProduct,
+} from "../controller/cart.controller.js";
 import { jwtAuth } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/add/:product_id").patch(jwtAuth, addToCart);
+router.route("/delete/:product_id").delete(jwtAuth, removeCartProduct);
+router.route("/decrease/:product_id").delete(jwtAuth, removeQuantityOfProduct);
+
 router.route("/get").get(jwtAuth, getCartProduct);
 
 export default router;
